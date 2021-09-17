cdef0a188bcfc54a37eb646ed88bb4fe994587e5
KLamdan <git.lamdan@gmail.com>
2021-09-17 18:36:33 +0300

    add firefox webDriver and first test

--- src/test/java/com/jenkins_test/app/AppTest.java
+++ src/test/java/com/jenkins_test/app/AppTest.java
@@ -1,21 +1,38 @@
 package com.jenkins_test.app;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    WebDriver webdriver;
+    private static final String MainPage = "https://www.google.com/";
+    public AppTest(){
+        webdriver = new FirefoxDriver();
     }
+
+   @BeforeSuite
+   private void chekDriver(){
+       assertNotNull(webdriver);
+   }
+   @BeforeTest
+   public void loadMainPage() { webdriver.get(AppTest.MainPage); }
+
+    @Test
+    public void first(){
+        assertEquals(webdriver.getCurrentUrl(), AppTest.MainPage); }
+
+    @AfterSuite
+    public void closeDriver(){ webdriver.quit(); }
+
 }
